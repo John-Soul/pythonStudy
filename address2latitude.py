99a5c4432440d8a5339e7a3da429d148f76c33fe
jianghao <jianghao@1hhd.com>
2019-12-30 20:22:32 +0800

    First commit

---
+++ address2latitude.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+import json
+import codecs
+
+df = pd.read_excel('C:/Users/chian/Desktop/address2latitude/qianniao.xls')
+data = df['address'].values
+son = data[:1]
+errorCount = 0
+
+def address2latitude(item):
+    if item != 0:
+        print(item)
+        url = "http://api.map.baidu.com/geocoding/v3/"
+        params = {
+            'ak': 'Bz0GWjvzAe95YRb9y5pW3UdYwngbTiUN',
+            'output': 'json',
+            # 'callback': 'showLocation',
+            'address': item
+        }
+        r = requests.get(url, params=params)
+        obj = json.load(r.text)
+        if obj.status == 0:
+            print(obj.result.loaction)
+
+for index in range(len(son)):
+    address2latitude(son[index])
